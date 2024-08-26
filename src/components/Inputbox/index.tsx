71a1087c08f206efa17d94cb9ad0b072c8891164
y0001ha <yyy96110@naver.com>
2024-08-26 09:51:58 +0900

    20240826 09:50

--- src/components/Inputbox/index.tsx
+++ src/components/Inputbox/index.tsx
@@ -5,15 +5,19 @@ interface Props {
     label: string;
     type: 'text' | 'password';
     placeholder: string;
+    value: string;
     buttonName?: string;
 }
 
-export default function InputBox({ label, type, placeholder, buttonName}: Props) {
+export default function InputBox({ label, type, placeholder, value, buttonName}: Props) {
+
+    // 이름
+
     return (
         <div className="input-box">
             <div className="label">{label}</div>
             <div className="input-area">
-                <input type={type} placeholder={placeholder} />
+                <input value={value} type={type} placeholder={placeholder} onChange={}/>
                 {buttonName && <div className="input-button disable">{buttonName}</div>}
             </div>
             <div className="message"></div>
