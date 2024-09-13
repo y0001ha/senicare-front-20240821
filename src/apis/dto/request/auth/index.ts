dcc0d81884c5a162febf77ac2da452398b40b6c7
y0001ha <yyy96110@naver.com>
2024-09-13 10:54:42 +0900

    20240913 10:50

--- src/apis/dto/request/auth/index.ts
+++ src/apis/dto/request/auth/index.ts
@@ -1,9 +1,11 @@
 import IdCheckRequestDto from "./id-check.request.dto";
 import telAuthRequestDto from "./tel-auth.request.dto";
 import TelAuthCheckRequestDto from "./tel-auth.check.request.dto";
+import SignUpRequestDto from "./sign-up.request.dto";
 
 export type {
     IdCheckRequestDto,
     telAuthRequestDto,
-    TelAuthCheckRequestDto
+    TelAuthCheckRequestDto,
+    SignUpRequestDto
 }
