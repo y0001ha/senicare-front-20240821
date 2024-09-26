39ceeda882900d8b4ba4428f48f35a82de47f950
y0001ha <yyy96110@naver.com>
2024-09-26 10:52:45 +0900

    20240926 10:50

---
+++ src/apis/dto/response/customer/get-customer-list.response.dto.ts
@@ -0,0 +1,8 @@
+import { Customer } from "src/types";
+import ResponseDto from "../response.dto";
+
+// interface: get customer list response body dto //
+export default interface GetCustomerListResponseDto extends ResponseDto{
+    customers: Customer[];
+
+}
